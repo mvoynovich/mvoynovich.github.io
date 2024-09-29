17c7639c99ba0677468d16a2754328f311dfab35
mvoynovich <143434817+mvoynovich@users.noreply.github.com>
2024-09-29 18:08:33 -0400

    test

--- js/displayNav.js
+++ js/displayNav.js
@@ -4,7 +4,7 @@ $(window).ready(function(){
   out += '<a href = "./index.html" class = "name">Matthew Voynovich</a>';
   out += '<nav class = "navbar">';
   out += '<a href = "/index.html#aboutContainer">Home</a>';
-  out += '<a href = "/Pages/Voynom-MatthewVoynovich-resume.html">Resume</a>';
+  out += '<a href = "Voynom-MatthewVoynovich-resume.html">Resume</a>';
   out += '<a href = "/index.html#projectsContainer">Projects</a>';
   out += '<a href = "/Pages/Contact.html">Contact</a>';
   out += '<a href = "https://github.com/mvoynovich" target ="_blank">Github</a>';
